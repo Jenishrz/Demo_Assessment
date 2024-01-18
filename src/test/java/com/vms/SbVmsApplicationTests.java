50c707bddc0ef04ba10c6d8e42f155e945a1b523
Jenishrz <jjenish.immanuel18@gmail.com>
2024-01-18 22:10:34 +0530

    First Commit

---
+++ src/test/java/com/vms/SbVmsApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.vms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbVmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
---
+++ src/test/java/com/vms/test/VMSControllerTest.java
@@ -0,0 +1,14 @@
+/*package com.vms.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VMSControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}*/
